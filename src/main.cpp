50528cacf574443792e9c417ba06fe7228dbb2dc
THEMANNICHOLAS <stingyblue@gmail.com>
2024-03-27 11:22:59 -0400

    map.cpp added for map

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,46 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 800u, 600u }, "COP3003 Project" };
+void userInput(sf::Keyboard);
+
+
+int main() {
+    auto window = sf::RenderWindow{{800u, 600u}, "COP3003 Project"};
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    sf::Sprite sprite;
+    sf::Texture texture;
+    texture.loadFromFile("C:\\Users\\sting\\repos\\COP3003Project\\assets\\apple.jpg");
+    sprite.setTexture(texture);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
+        //Renders and shows object, constantly refreshing each frame
         window.clear();
+        window.draw(sprite);
         window.display();
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+
+        }
+
+
+
+
+
     }
 }
---
+++ src/map.cpp
@@ -0,0 +1,6 @@
+//
+// Created by sting on 3/27/2024.
+//
+#include <array>
+#include <SFML/Graphics.hpp>
+
